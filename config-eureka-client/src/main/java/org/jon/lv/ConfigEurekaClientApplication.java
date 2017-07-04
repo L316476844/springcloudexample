ae03b52193c5ad54ce167df97937d7b4234fd5e6
lvbin <lvbin@oriental-finance.com>
2017-07-04 16:10:35 +0800

    依赖eureka高可用配置中心

--- config-eureka-client/src/main/java/org/jon/lv/ConfigEurekaClientApplication.java
+++ config-eureka-client/src/main/java/org/jon/lv/ConfigEurekaClientApplication.java
@@ -3,6 +3,8 @@ package org.jon.lv;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
  * version V1.0.0
  */
 
-@RestController
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RefreshScope
 public class ConfigEurekaClientApplication {
 
     @Value("${jon}")
